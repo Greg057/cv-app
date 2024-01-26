999a77a5a83c4f260e9bf07e1541de3fb9c58aa4
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-26 20:25:09 +0100

    Change description from input to text Area

--- src/components/ExperienceInfo.jsx
+++ src/components/ExperienceInfo.jsx
@@ -12,7 +12,7 @@ export default function ExperienceInfo ({ experienceInfo, id, setExperienceInfo,
                     <InputField title="Start Date" info={experienceInfo} valueToChange="startDate" id={id} setFunction={setExperienceInfo} />
                     <InputField title="End Date" info={experienceInfo} valueToChange="endDate" id={id} setFunction={setExperienceInfo} />
                     <InputField title="Location" info={experienceInfo} valueToChange="location" id={id} setFunction={setExperienceInfo} />
-                    <InputField title="Description" info={experienceInfo} valueToChange="description" id={id} setFunction={setExperienceInfo} />
+                    <InputField title="Description" info={experienceInfo} valueToChange="description" id={id} setFunction={setExperienceInfo} textArea="true"/>
                     <div className="edit-buttons">
                         <button onClick={() => removeExperience(id)}><span className="material-symbols-outlined">delete</span>Delete</button>
                         <button className="close" onClick={() => handleClick(id)}><span className="material-symbols-outlined">close</span>Close</button>
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,13 +1,19 @@
 import "../styles/input-field.css"
 
-export default function InputField ({ title, info, setFunction, valueToChange, id = false }) {
+export default function InputField ({ title, info, setFunction, valueToChange, id = false, textArea = false }) {
     return (
         <div className="input-field">
             <h4>{title}</h4>
-            <input type="text" value={info[valueToChange]} placeholder={"Enter " + title} 
-                    onChange={id === false
-                                    ? (e) => setFunction({...info, [valueToChange]: e.target.value}) 
-                                    : (e) => setFunction(id, [valueToChange], e.target.value)}/>
+            {textArea ? 
+            <textarea value={info[valueToChange]} placeholder={"Enter " + title} rows={7}
+                        onChange={(e) => setFunction(id, [valueToChange], e.target.value)}></textarea>
+            : <input type="text" value={info[valueToChange]} placeholder={"Enter " + title} 
+                        onChange={id === false
+                            ? (e) => setFunction({...info, [valueToChange]: e.target.value}) 
+                            : (e) => setFunction(id, [valueToChange], e.target.value)}/>
+            
+        }
+            
         </div>
     )
 }
