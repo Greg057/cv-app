f277aaef8c318c318db7bf4b65c4dc9c55fdd1c2
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-26 14:46:50 +0100

    Add logic to have one education edit section open at a time

--- src/components/EditSection.jsx
+++ src/components/EditSection.jsx
@@ -2,14 +2,26 @@ import PersonalInfo from "./PersonalInfo"
 import EducationInfo from "./EducationInfo"
 import ExperienceInfo from "./ExperienceInfo"
 import "../styles/info-container.css"
+import { useState } from "react";
 
 
 export default function EditSection ({ personalInfo, setPersonalInfo, educationInfo, setEducationInfo, experienceInfo, setExperienceInfo }) {
+    const [activeIndex, setActiveIndex] = useState();
+
+    function handleClick(id) {
+        activeIndex === id ? setActiveIndex() : setActiveIndex(id)
+    }
+    
     return (
         <div className="edit-section">
             <PersonalInfo personalInfo={personalInfo} setPersonalInfo={setPersonalInfo} />
-            {educationInfo.map(education => 
-                <EducationInfo educationInfo={education} key={education.id} id={education.id} setEducationInfo={setEducationInfo}/> )}
+
+            <div className="info-container">
+                <h2>Education</h2>
+                {educationInfo.map(education => 
+                    <EducationInfo educationInfo={education} key={education.id} id={education.id} setEducationInfo={setEducationInfo} isActive={activeIndex === education.id} handleClick={handleClick}/> )}
+            </div>
+
             {experienceInfo.map(experience => 
                 <ExperienceInfo experienceInfo={experience} key={experience.id} id={experience.id} setExperienceInfo={setExperienceInfo}/> )}
         </div>
--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,14 +1,22 @@
 import InputField from "./InputField";
 
-export default function EducationInfo ({ educationInfo, id, setEducationInfo }) {
+
+export default function EducationInfo ({ educationInfo, id, setEducationInfo, isActive, handleClick }) {
+    
     return (
-        <div className="info-container">
-            <h2>Education</h2>
-            <InputField title="School" info={educationInfo} valueToChange="school" id={id} setFunction={setEducationInfo} />
-            <InputField title="Degree" info={educationInfo} valueToChange="degree" id={id} setFunction={setEducationInfo} />
-            <InputField title="Start date" info={educationInfo} valueToChange="startDate" id={id} setFunction={setEducationInfo} />
-            <InputField title="End date" info={educationInfo} valueToChange="endDate" id={id} setFunction={setEducationInfo} />
-            <InputField title="Location" info={educationInfo} valueToChange="location" id={id} setFunction={setEducationInfo} />
+        <div>
+            <div className="edit-item" onClick={() => !isActive && handleClick(id)}>
+                {isActive ?
+                <>
+                    <InputField title="School" info={educationInfo} valueToChange="school" id={id} setFunction={setEducationInfo} />
+                    <InputField title="Degree" info={educationInfo} valueToChange="degree" id={id} setFunction={setEducationInfo} />
+                    <InputField title="Start date" info={educationInfo} valueToChange="startDate" id={id} setFunction={setEducationInfo} />
+                    <InputField title="End date" info={educationInfo} valueToChange="endDate" id={id} setFunction={setEducationInfo} />
+                    <InputField title="Location" info={educationInfo} valueToChange="location" id={id} setFunction={setEducationInfo} />
+                    <button onClick={() => handleClick(id)}>Close</button>
+                </>
+                : <h3>{educationInfo.school}</h3>}
+            </div>
         </div>
     )
 }
